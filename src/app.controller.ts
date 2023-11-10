c4f7e1688e5c08f92786399e2d9a63b1fc30d9ab
Gabriel Thome <eugabrielthome@gmail.com>
2023-11-09 22:27:10 -0300

    create route

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,24 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { PrismaService } from './database/prisma.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private prisma: PrismaService,
+  ) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('create')
+  async getHello(){
+    const member = await this.prisma.teamMember.create({
+      data: {
+        id: 'captain',
+        name: 'Rafa',
+        function: 'Fighter'
+      },
+    });
+    return {
+      member,
+    };
   }
 }
